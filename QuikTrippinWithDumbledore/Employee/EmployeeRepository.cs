1fc8cfe7dbdd437d539cc05f38eeb079896c80ec
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-08-12 18:58:49 -0500

    - created CurrQtrRetailSales and AnnualRetailSales

--- QuikTrippinWithDumbledore/Employee/EmployeeRepository.cs
+++ QuikTrippinWithDumbledore/Employee/EmployeeRepository.cs
@@ -10,42 +10,42 @@ namespace QuikTrippinWithDumbledore.Employee
     {
         static List<DistrictManager> _districtManagers = new List<DistrictManager>()
         {
-            new DistrictManager{FirstName="Chris", LastName="Meffley", EmployeeID=383938, RetailSales=135000.46m},
-            new DistrictManager{FirstName="Matthew", LastName="Gonzales", EmployeeID=458945, RetailSales=13000.76m},
-            new DistrictManager{FirstName="Jim", LastName="Conner", EmployeeID=238723, RetailSales=14000.99m},
-            new DistrictManager{FirstName="Ellie", LastName="Walker", EmployeeID=129812, RetailSales=135000.55m},
+            new DistrictManager{FirstName="Chris", LastName="Meffley", EmployeeID=383938, CurrQtrRetailSales=2344.45m, AnnualRetailSales=135000.46m},
+            new DistrictManager{FirstName="Matthew", LastName="Gonzales", EmployeeID=458945, CurrQtrRetailSales=3434.45m, AnnualRetailSales=13000.76m},
+            new DistrictManager{FirstName="Jim", LastName="Conner", EmployeeID=238723, CurrQtrRetailSales=4334.45m, AnnualRetailSales=14000.99m},
+            new DistrictManager{FirstName="Ellie", LastName="Walker", EmployeeID=129812, CurrQtrRetailSales=4234.45m, AnnualRetailSales=135000.55m},
         };
 
         static List<StoreManager> _storeManagers = new List<StoreManager>()
         {
-            new StoreManager{FirstName="Ben", LastName="Jarrett", EmployeeID=111111, RetailSales=74686.33m},
-            new StoreManager{FirstName="Casey", LastName="Walker", EmployeeID=222222, RetailSales=75894.55m},
-            new StoreManager{FirstName="Chie", LastName="Stroud", EmployeeID=333333, RetailSales=85874.54m},
-            new StoreManager{FirstName="Dani", LastName="Crosby", EmployeeID=444444, RetailSales=83746.34m},
+            new StoreManager{FirstName="Ben", LastName="Jarrett", EmployeeID=111111, CurrQtrRetailSales=2234.45m, AnnualRetailSales=74686.33m},
+            new StoreManager{FirstName="Casey", LastName="Walker", EmployeeID=222222, CurrQtrRetailSales=2934.45m, AnnualRetailSales=75894.55m},
+            new StoreManager{FirstName="Chie", LastName="Stroud", EmployeeID=333333, CurrQtrRetailSales=2424.45m, AnnualRetailSales=85874.54m},
+            new StoreManager{FirstName="Dani", LastName="Crosby", EmployeeID=444444, CurrQtrRetailSales=2264.45m, AnnualRetailSales=83746.34m},
         };
 
         static List<AssistantManager> _assistantManagers = new List<AssistantManager>()
         {
-            new AssistantManager{FirstName="Martin", LastName="Sisk", EmployeeID=11111, RetailSales=4746.54m},
-            new AssistantManager{FirstName="Gabby", LastName="Tobermann", EmployeeID=22222, RetailSales=7575.45m},
-            new AssistantManager{FirstName="John", LastName="Maple", EmployeeID=33333, RetailSales=8585.47m},
-            new AssistantManager{FirstName="Holly", LastName="Parsons", EmployeeID=44444, RetailSales=9898.45m},
+            new AssistantManager{FirstName="Martin", LastName="Sisk", EmployeeID=11111, CurrQtrRetailSales=1234.45m, AnnualRetailSales=4746.54m},
+            new AssistantManager{FirstName="Gabby", LastName="Tobermann", EmployeeID=22222, CurrQtrRetailSales=1334.45m, AnnualRetailSales=7575.45m},
+            new AssistantManager{FirstName="John", LastName="Maple", EmployeeID=33333, CurrQtrRetailSales=1854.45m, AnnualRetailSales=8585.47m},
+            new AssistantManager{FirstName="Holly", LastName="Parsons", EmployeeID=44444, CurrQtrRetailSales=1454.45m, AnnualRetailSales=9898.45m},
         };
         
         static List<Associate> _associates = new List<Associate>()
         {
-            new Associate{FirstName="Hunter", LastName="Juneau", EmployeeID=1, RetailSales=234.45m},
-            new Associate{FirstName="Jesse", LastName="Robinson", EmployeeID=2, RetailSales=994.38m},
-            new Associate{FirstName="Juan", LastName="Davila", EmployeeID=3, RetailSales=848.57m},
-            new Associate{FirstName="Katy", LastName="Fry", EmployeeID=4, RetailSales=748.57m},
-            new Associate{FirstName="Lindsey", LastName="Satterfield", EmployeeID=5, RetailSales=234.54m},
-            new Associate{FirstName="Mitchell", LastName="Crumbley", EmployeeID=6, RetailSales=737.37m},
-            new Associate{FirstName="Nathan", LastName="Keith", EmployeeID=7, RetailSales=983.47m},
-            new Associate{FirstName="Rob", LastName="Cole", EmployeeID=8, RetailSales=847.35m},
-            new Associate{FirstName="Sean", LastName="Rossettie", EmployeeID=9, RetailSales=647.35m},
-            new Associate{FirstName="Sara", LastName="Schoonover", EmployeeID=10, RetailSales=456.45m},
-            new Associate{FirstName="Honey-Rae", LastName="Swan", EmployeeID=11, RetailSales=234.55m},
-            new Associate{FirstName="Tad", LastName="Sekeres", EmployeeID=12, RetailSales=200.34m},
+            new Associate{FirstName="Hunter", LastName="Juneau", EmployeeID=1, CurrQtrRetailSales=234.45m, AnnualRetailSales=1746.54m},
+            new Associate{FirstName="Jesse", LastName="Robinson", EmployeeID=2, CurrQtrRetailSales=994.38m, AnnualRetailSales=2746.54m},
+            new Associate{FirstName="Juan", LastName="Davila", EmployeeID=3, CurrQtrRetailSales=848.57m, AnnualRetailSales=2346.54m},
+            new Associate{FirstName="Katy", LastName="Fry", EmployeeID=4, CurrQtrRetailSales=748.57m, AnnualRetailSales=1546.54m},
+            new Associate{FirstName="Lindsey", LastName="Satterfield", EmployeeID=5, CurrQtrRetailSales=234.54m, AnnualRetailSales=2946.54m},
+            new Associate{FirstName="Mitchell", LastName="Crumbley", EmployeeID=6, CurrQtrRetailSales=737.37m, AnnualRetailSales=2246.54m},
+            new Associate{FirstName="Nathan", LastName="Keith", EmployeeID=7, CurrQtrRetailSales=983.47m, AnnualRetailSales=3246.54m},
+            new Associate{FirstName="Rob", LastName="Cole", EmployeeID=8, CurrQtrRetailSales=847.35m, AnnualRetailSales=2246.54m},
+            new Associate{FirstName="Sean", LastName="Rossettie", EmployeeID=9, CurrQtrRetailSales=647.35m, AnnualRetailSales=2146.54m},
+            new Associate{FirstName="Sara", LastName="Schoonover", EmployeeID=10, CurrQtrRetailSales=456.45m, AnnualRetailSales=1946.54m},
+            new Associate{FirstName="Honey-Rae", LastName="Swan", EmployeeID=11, CurrQtrRetailSales=234.55m, AnnualRetailSales=2246.54m},
+            new Associate{FirstName="Tad", LastName="Sekeres", EmployeeID=12, CurrQtrRetailSales=200.34m, AnnualRetailSales=1346.54m},
         };
 
         public void AddAssociate(Associate associate)
@@ -108,6 +108,7 @@ namespace QuikTrippinWithDumbledore.Employee
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int EmployeeID { get; set; }
-        public decimal RetailSales { get; set; } //don't forget to put a "m" after the number
+        public decimal CurrQtrRetailSales { get; set; } //don't forget to put a "m" after the number
+        public decimal AnnualRetailSales { get; set; }
     }
 }
